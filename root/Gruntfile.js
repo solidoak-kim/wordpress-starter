fa471e1217c9a9cb2b8a7650e3ee222587dca0da
Oak Kim <solidoak-kim@gmail.com>
2015-07-08 14:14:44 -0400

    adding sass files

---
+++ root/Gruntfile.js
@@ -0,0 +1,84 @@
+'use strict';
+
+module.exports = function(grunt){
+
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      options: {
+        livereload: true
+      },
+      php: {
+        files: ['**/*.php']
+      },
+      css: {
+        files: ['**/*.css']
+      },
+      stylus: {
+        files: ['styles/stylus/*.styl'],
+        tasks: ['stylus:compile']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      typescript: {
+        files: 'scripts/typescript/*.ts',
+        tasks: ['typescript']
+      }
+    },
+
+    // Compoass Sass compile task
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'styles/sass',
+          cssDir: 'styles/css',
+          environment: 'production'
+        }
+      },
+      dev: {
+        options: {
+          sassDir: 'styles/sass',
+          cssDir: 'styles/css'
+        }
+      }
+    },
+
+    // Stylus compile task
+    stylus: {
+      compile: {
+        options: {
+          paths: ['styles/stylus']
+        },
+        files: {
+          'styles/css/main-stylus.css': 'styles/stylus/main.styl'
+        }
+      }
+    },
+
+    // Typescript compile task
+    typescript: {
+      base: {
+        src: ['scripts/typescript/*.ts'],
+        dest: 'scripts/js',
+        options: {
+          module: 'commonjs',
+          target: 'ES3',
+          sourceMap: true,
+          removeComments: true,
+        }
+      }
+    }
+
+  });
+
+  // Load the required plugins
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-typescript');
+
+  // Default task(s).
+  grunt.registerTask('dev', ['watch', 'stylus', 'compass:dev']);
+
+};

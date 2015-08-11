b5f93f57baa7fb2bad9f797b75faf50be24e5489
Oak Kim <solidoak-kim@gmail.com>
2015-08-10 22:02:23 -0400

    updated gruntfile

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -165,7 +165,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-typescript');
   grunt.loadNpmTasks('grunt-scss-lint');
-  grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-bower-task');
 
   // Register tasks
   grunt.registerTask('dev', ['compass:dev', 'jshint', 'scsslint', 'bower:install', 'watch']);

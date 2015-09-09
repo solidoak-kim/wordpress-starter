06654f00807c61c4c6e282a6dd3440fb1b2b0e77
Oak Kim <solidoak-kim@gmail.com>
2015-09-09 17:46:40 -0400

    cleaning up functions file; cleaning up header.php

--- root/wp-content/themes/wp-myplay-store/header.php
+++ root/wp-content/themes/wp-myplay-store/header.php
@@ -1,49 +1,51 @@
 <!DOCTYPE html>
-<!--[if IE 7]><html class="ie ie7" <?php language_attributes(); ?>><![endif]-->
-<!--[if IE 8]><html class="ie ie8" <?php language_attributes(); ?>><![endif]-->
-<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>><![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]>
 <html <?php language_attributes(); ?> xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#"
-                                      xmlns:fb="http://www.facebook.com/2008/fbml">
+xmlns:fb="http://www.facebook.com/2008/fbml">
 <!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
-	<meta name="viewport" content="width=device-width, user-scalable=no"/>
-	<meta name="description" content="<?php bloginfo( 'description' ); ?>"/>
-	<meta name="keywords" content="<?php bloginfo( 'name' ); ?>"/>
-	<meta property="og:type" content="website"/>
-	<meta property="og:site_name" content="<?php bloginfo( 'name' ); ?>"/>
-	<meta property="fb:app_id" content="270065846467161"/>
+  <meta charset="<?php bloginfo( 'charset' ); ?>"/>
+  <meta name="viewport" content="width=device-width, user-scalable=no"/>
+  <meta name="description" content="<?php bloginfo( 'description' ); ?>"/>
+  <meta name="keywords" content="<?php bloginfo( 'name' ); ?>"/>
+  <meta property="og:type" content="website"/>
+  <meta property="og:site_name" content="<?php bloginfo( 'name' ); ?>"/>
+  <meta property="fb:app_id" content="270065846467161"/>
 
-	<title><?php echo _rgnrtrWPTitle(); ?></title>
+  <title><?php echo _rgnrtrWPTitle(); ?></title>
 
-	<?php if ( is_single() ) : global $post;
-		setup_postdata( $post ); ?>
-		<meta property="og:title" content="<?php the_title(); ?>"/>
-		<meta property="og:url" content="<?php the_permalink( $post - ID ); ?>"/>
-		<meta property="og:image" content="<?php echo _rgnrtrGetImageURL( $post - ID ); ?>"/>
-		<meta property="og:description" content="<?php echo _rgnrtrExcerpt( 20, false, '', true ); ?> "/>
+  <?php if ( is_single() ) : global $post;
+    setup_postdata( $post ); ?>
+    <meta property="og:title" content="<?php the_title(); ?>"/>
+    <meta property="og:url" content="<?php the_permalink( $post->ID ); ?>"/>
+    <meta property="og:image" content=""/>
+    <meta property="og:description" content=""/>
 
-	<?php else : ?>
-		<meta property="og:title" content="<?php bloginfo( 'name' ); ?>"/>
-		<meta property="og:url" content="<?php bloginfo( 'url' ); ?>"/>
-		<meta property="og:image" content="<?php echo _rgnrtrGetImageURL( $post - ID ); ?>"/>
-		<meta property="og:description" content="<?php echo bloginfo( 'description' ); ?>"/>
-	<?php endif; ?>
+  <?php else : ?>
+    <meta property="og:title" content="<?php bloginfo( 'name' ); ?>"/>
+    <meta property="og:url" content="<?php bloginfo( 'url' ); ?>"/>
+    <meta property="og:image" content=""/>
+    <meta property="og:description" content="<?php echo bloginfo( 'description' ); ?>"/>
+  <?php endif; ?>
 
-	<link rel="shortcut icon" href="<?php bloginfo( 'template_directory' ); ?>/images/favicon.ico" type="image/x-icon"/>
-	<link rel="profile" href="http://gmpg.org/xfn/11"/>
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"/>
+  <?php if ( file_exists( bloginfo( 'template_directory' ) . '/images/favicon.ico' ) ) : ?>
+    <link rel="shortcut icon" href="<?php bloginfo( 'template_directory' ); ?>/images/favicon.ico" type="image/x-icon"/>
+  <?php endif; ?>
+  <link rel="profile" href="http://gmpg.org/xfn/11"/>
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"/>
 
-	<!--[if lt IE 9]><script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script><![endif]-->
-
-	<?php wp_head(); ?>
+  <?php wp_head(); ?>
 </head>
 
-<body <?php _rgnrtrIOSClass( body_class() ); ?> >
+<body <?php body_class(); ?>>
 <div id="site-container" class="site-container">
 
-	<div id="header-container" class="header-container">
-		<?php get_header('main'); ?>
-	</div>
+  <div id="header-container" class="header-container">
+    <?php get_header( 'main' ); ?>
+  </div>
 
-	<main id="main-container" class="main-container" role="main">
+  <main id="main-container" class="main-container" role="main">
